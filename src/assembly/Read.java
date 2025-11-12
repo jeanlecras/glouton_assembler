67b6677e5fdf9037fed019e44960126203d679e4
ishaac <xf1r3Z@gmail.com>
2025-11-12 20:54:05 +0100

    question11

--- src/assembly/Read.java
+++ src/assembly/Read.java
@@ -34,10 +34,28 @@ public class Read implements Sequence{
 		return sb.toString();
 	}
 	
+	public static boolean nearlyEquals(String s1, String s2) { 
+		if (s1 == null || s2 == null || s1.length() != s2.length()) {
+			return false;
+		}
+
+		int mismatchcount = 0;
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				mismatchcount++;
+				if (mismatchcount > 1) {
+					return false;
+				}
+			}
+		}
+		return mismatchcount <=1;
+	}
+	
 	public static void main(String[] args) {
 		Read r1 = new Read("azertyuiop");
 		Read r2 = new Read("ertyuiopuui");
-	//	System.out.println(r1.bestOverlap(r2));
+		//	System.out.println(r1.bestOverlap(r2));
+		
 	}
 	
 	public Read() {
